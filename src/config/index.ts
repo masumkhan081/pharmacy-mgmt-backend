7ba27f3ad5a7ca2f2ca11bb4eecae056e61207a1
masumkhan081 <masumkhan081@gmail.com>
2025-01-09 17:23:31 +0600

    works on staff and user

--- src/config/index.ts
+++ src/config/index.ts
@@ -26,7 +26,7 @@ const config: Config = {
   dbName: "pharmacy-management",
   dbUrl:
     process.env.DB_URL ||
-    "mongodb+srv://masumkhan:pddrgj3q@cluster0.48jxv.mongodb.net/",
+    "mongodb+srv://masumkhan:pddrgj3q@drugs.dvfzpkl.mongodb.net/",
   tokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET || "i-act-as-token-secret",
   refreshTokenSecret:
     process.env.JWT_REFRESH_TOKEN_SECRET || "i-act-as-refresh-token-secret",
